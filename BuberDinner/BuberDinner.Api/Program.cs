816e9384ce9cd563b1c5b8b5275d1a45fb34af9c
salaheddineobt <salaheddine.ouladbentaib@gmail.com>
2022-08-05 09:52:14 +0100

    save changes

--- BuberDinner/BuberDinner.Api/Program.cs
+++ BuberDinner/BuberDinner.Api/Program.cs
@@ -1,8 +1,12 @@
-using  BuberDinner.Application.Services.Authentication;
+using BuberDinner.Application;
+using BuberDinner.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
-    builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+    builder.Services
+        .AddApplication()
+        .AddInfrastructure();
+        
     builder.Services.AddControllers();
 }
 
---
+++ BuberDinner/BuberDinner.Application/DependencyInjection.cs
@@ -0,0 +1,14 @@
+using Microsoft.Extensions.DependencyInjection;
+using BuberDinner.Application.Services.Authentication;
+
+namespace BuberDinner.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<IAuthenticationService, AuthenticationService>();
+
+        return services;
+    }
+}
